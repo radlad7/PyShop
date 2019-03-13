696bd8f5f5be52626d3241071484e61e5dea9ac3
Ray <radlad@protonmail.com>
2019-03-12 21:32:47 -0400

    Implemented bootstrap 4 header, navbar, footer, styled category and product pages, setup fontawesome and added a Paginator to products page.

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
@@ -11,6 +12,9 @@ class Category(models.Model):
 		ordering = ('name',)
 		verbose_name = 'category'
 		verbose_name_plural = 'categories'
+
+	def get_url(self):
+		return reverse('shop:products_by_category', args=[self.slug])		
 
 	def __str__(self):
 		return '{}'.format(self.name)
@@ -31,6 +35,9 @@ class Product(models.Model):
 		ordering = ('name',)
 		verbose_name = 'product'
 		verbose_name_plural = 'products'
+
+	def get_url(self):
+		return reverse('shop:ProdCatDetail', args=[self.category.slug, self.slug])
 
 	def __str__(self):
 		return '{}'.format(self.name)
